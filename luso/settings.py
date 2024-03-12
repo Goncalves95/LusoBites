3bde5bcb3652850768662682d66f843f2771642d
Goncalves95 <138715742+Goncalves95@users.noreply.github.com>
2024-03-12 20:02:46 +0100

    Fixing bug

--- luso/settings.py
+++ luso/settings.py
@@ -30,7 +30,8 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
-                 '.herokuapp.com']
+                 '.herokuapp.com',
+                 "luso-bites-9e7b470f6cac.herokuapp.com"]
 
 
 # Application definition
